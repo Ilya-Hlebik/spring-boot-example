4c805a5e38f710a20a6a943d8dea139bdff22db5
Ilya-Hlebik <felix.2894@mail.ru>
2021-10-20 17:46:07 +0300

    Initial config for UNIT-37

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -1,16 +1,14 @@
 package org.example.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String test(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "testview";
+    public String test(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+        return name;
     }
 }
